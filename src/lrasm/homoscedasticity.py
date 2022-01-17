253c41160c0f4866781375a1cddf46a8cbd2bf90
Song Yang <syang003@student.ubc.ca>
2022-01-17 15:24:49 -0800

    changed function name and documentation

--- src/lrasm/homoscedasticity.py
+++ src/lrasm/homoscedasticity.py
@@ -1,13 +1,10 @@
 
-def test_homoscedacity(model, X, y):
+def homoscedasticity_test(X, y):
     """This function recieves a linear regression model and outputs a
     scatter plot figure of residuals plotted against fitted values.
 
     Parameters
     ----------
-    model : sklearn.linear_model.base.LinearRegression
-        Scikit-learn Linear regression model
-
     X : pd.Dataframe
         Dataframe containing exploratory variable data
 
@@ -21,6 +18,6 @@ def test_homoscedacity(model, X, y):
 
     Examples
     --------
-    >>> test_homoscedacity(LinearRegression(), X_train, y_train) 
+    >>> homoscedasticity_test(X_train, y_train) 
 
     """
