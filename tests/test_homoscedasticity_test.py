e110c4fbf6f222c5cec17f3fdd07b9c39785ddb7
Song Yang <syang003@student.ubc.ca>
2022-01-17 21:15:05 -0800

    added data type tests for homoscedacity

---
+++ tests/test_homoscedasticity_test.py
@@ -0,0 +1,25 @@
+from lrasm.lrasm import homoscedasticity_test
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import linear_model
+from matplotlib.pyplot import figure
+from scipy import stats
+
+def test_homoscedasticity_test():
+    """Test homoscedasticity test outputs from dataset"""
+    X_proper = pd.DataFrame({"head": [1,2,3,3,5,8,7]})
+    X_str_df = pd.DataFrame({"head": ["str",2,3,4,5,6,7]})
+    X_series = pd.Series([1,2,3,4,5,6,7])
+
+    y_proper = pd.Series([1,2,3,4,5,6,7])
+    y_str_df = pd.Series(["str",2,3,4,5,6,7])
+
+    df, plot = homoscedasticity_test(X_str_df, y_proper)
+    assert df is None and plot is None
+
+    df, plot = homoscedasticity_test(X_series, y_proper)
+    assert df is None and plot is None
+
+    df, plot = homoscedasticity_test(X_proper, y_str_df)
+    assert df is None and plot is None
